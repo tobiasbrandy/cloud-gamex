07dc3debba208984188eafcdb20749b6c9402d1a
Joaco Lega <jlegammare@itba.edu.ar>
2022-06-18 17:47:38 -0300

    RDS instances with read replicas :D

--- aws/modules/persistance/main.tf
+++ aws/modules/persistance/main.tf
@@ -1,25 +1,36 @@
-resource "aws_db_instance" "uddin-sameed" {
-  identifier             = "uddin-sameed"
-  instance_class         = "db.t2.micro"
-  allocated_storage      = 5
-  engine                 = "postgres"
+resource "aws_db_subnet_group" "database" {
+  name = "my-test-database-subnet-group"
+  subnet_ids = var.persistance_subnets
+}
+
+resource "aws_db_instance" "primary_db" {
+  allocated_storage    = 5
+  identifier           = "primary"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "user"
+  password             = "password8char"
+  db_subnet_group_name = "${aws_db_subnet_group.database.id}"
+/* availability_zone=  */
+  vpc_security_group_ids    = ["${aws_security_group.rds.id}"]
   skip_final_snapshot    = true
-  publicly_accessible    = true
-  username               = "sameed"
-  password               = "random_string.uddin-db-password.result}"
+
 #Backups are required in order to create a replica
   maintenance_window      = "Mon:00:00-Mon:03:00"
   backup_window           = "03:00-06:00"
   backup_retention_period = 1
 }
-resource "aws_db_instance" "uddin-sameed-read" {
-  identifier             = "uddin-sameed-read"
-  replicate_source_db    = aws_db_instance.uddin-sameed.identifier ## refer to the master instance
-  instance_class         = "db.t2.micro"
+resource "aws_db_instance" "read_replica" {
+  count = length(var.persistance_subnets)
+  identifier             = "replic-${count.index}"
+  replicate_source_db    = "primary"
+  instance_class         = "db.t3.micro"
+  vpc_security_group_ids    = ["${aws_security_group.rds.id}"]
   allocated_storage      = 5
   skip_final_snapshot    = true
-  publicly_accessible    = true
 # disable backups to create DB faster
   backup_retention_period = 0
 }
+
 
--- config.tfvars
+++ config.tfvars
@@ -1,8 +1,8 @@
 
 ssh_key_path      = "~/.ssh/id_rsa.pub"
 my_ips            = ["200.123.140.195/32", "181.29.41.98/32"]
-app_domain        = "cloud.tobiasbrandy.com"
 authorized_role   = "LabRole"
+app_domain       = "cloud-jl3.tobiasbrandy.com"
 
 # AWS
 aws_region        = "us-east-1"
