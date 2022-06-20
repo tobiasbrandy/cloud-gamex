01e256fabc1a42b89723dbf569928fa717fc5d58
Tobias Brandy <tbrandy@itba.edu.ar>
2022-06-20 06:57:35 -0300

    Enable Performance Insights to detect potential problems

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -30,7 +30,9 @@ resource "aws_db_instance" "primary_db" {
   maintenance_window      = "Mon:00:00-Mon:03:00"
   backup_window           = "03:00-06:00"
   backup_retention_period = 1
-  storage_encrypted       = true 
+
+  storage_encrypted       = true
+  performance_insights_enabled = true
 
   skip_final_snapshot     = true
   apply_immediately       = true
@@ -51,7 +53,8 @@ resource "aws_db_instance" "read_replica" {
   
   # Read Replicas don't backup
   backup_retention_period = 0
-  storage_encrypted       = true 
+  storage_encrypted       = true
+  performance_insights_enabled = true
 
   skip_final_snapshot     = true
   apply_immediately       = true
