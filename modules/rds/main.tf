5adc4929b4e6967da2ab5ee8679ed22a18a3e58a
Tobias Brandy <tbrandy@itba.edu.ar>
2022-06-20 06:58:49 -0300

    RDS Cluster and RDS instance should have backup retention longer than default 1 day

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -29,7 +29,7 @@ resource "aws_db_instance" "primary_db" {
   # Backups are required in order to create a replica
   maintenance_window      = "Mon:00:00-Mon:03:00"
   backup_window           = "03:00-06:00"
-  backup_retention_period = 1
+  backup_retention_period = 5
 
   storage_encrypted       = true
   performance_insights_enabled = true
