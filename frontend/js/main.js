5c90651f83b351f08fb88aa06c8493ef7a80817b
Joaco Lega <jlegammare@itba.edu.ar>
2022-07-17 16:34:03 -0300

    fix main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,17 +1,14 @@
 async function getUsers(){
-    
     fetch('/api/db/users')
     .then(response=>response.json())
     .then(users => {
-        console.log(users)
-        // let ul = document.createElement('ul');
-        // let users_list = document.getElementById('users_list').append(ul)
-        // users.forEach(item => {
-        //     let li = document.createElement('li');
-        //     ul.appendChild(li);
-        //     li.innerHTML += item;
-        // });
-    
+        let ul = document.createElement('ul');
+        let users_list = document.getElementById('users_list').append(ul)
+        users.forEach(item => {
+            let li = document.createElement('li');
+            ul.appendChild(li);
+            li.innerHTML += item;
+        });
     })
     
     .catch(error =>console.log(error));
@@ -21,8 +18,11 @@ async function createUser(){
     let user_name = document.getElementById('name').value 
     if(user_name != ''){
         fetch('/api/db/user',{
-        method: 'POST',
-        body: JSON.stringify(user_name)
+            method: 'POST',
+            body: JSON.stringify(user_name),
+            headers:{
+                'Content-Type': 'application/json'
+            }
         })
         .then(response=>console.log(response))
         .catch(error => console.log(error))
