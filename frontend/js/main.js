2e1f10480207dd670527de26a79d2968712ebaf1
Joaco Lega <jlegammare@itba.edu.ar>
2022-07-17 16:42:08 -0300

    fix createUser()

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,24 +2,33 @@ async function getUsers(){
     fetch('/api/db/users')
     .then(response=>response.json())
     .then(users => {
+        let users_list = document.getElementById('users_list');
+        users_list.innerHTML = ''
+
         let ul = document.createElement('ul');
-        let users_list = document.getElementById('users_list').append(ul)
         users.forEach(item => {
             let li = document.createElement('li');
             ul.appendChild(li);
-            li.innerHTML += item;
+            li.innerHTML += item.name;
         });
+
+        users_list.append(ul)
+
     })
     
     .catch(error =>console.log(error));
 }
 
 async function createUser(){
-    let user_name = document.getElementById('name').value 
+    let user_name = document.getElementById('name').value;
+    data = {
+        name : user_name
+    };
+
     if(user_name != ''){
         fetch('/api/db/user',{
             method: 'POST',
-            body: JSON.stringify(user_name),
+            body: JSON.stringify(data),
             headers:{
                 'Content-Type': 'application/json'
             }
